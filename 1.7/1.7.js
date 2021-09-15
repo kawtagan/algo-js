2d3310ad155ca9132295ee47ce33d28b07ab9788
kawtagan <89374054+kawtagan@users.noreply.github.com>
2021-09-15 13:59:23 +0200

    Add files via upload

---
+++ 1.7/1.7.js
@@ -0,0 +1,12 @@
+const readlineSync = require("readline-sync");
+
+let shoeSize = '42';
+let BirthDay = '19';
+console.log("My size is " + shoeSize);
+console.log("My birthday is " + BirthDay);
+
+console.log(shoeSize * 2);
+console.log(84 + 5);
+console.log(89 * 50);
+console.log(4450 - BirthDay);
+console.log(4431 + 1766);
