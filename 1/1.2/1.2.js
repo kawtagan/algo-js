b1bab7ec440a2881fba0d77c4f34146deb0d4428
kawtagan <89374054+kawtagan@users.noreply.github.com>
2021-09-22 12:03:58 +0200

    Add files via upload

---
+++ 1/1.2/1.2.js
@@ -0,0 +1,9 @@
+const readlineSync = require("readline-sync");
+
+let name = "Gérard";
+let FirstName = "Lambert";
+let city = "Paris";
+
+console.log("Tu t'appelles" +  name +  FirstName);
+
+console.log("et tu habites à" + city);
