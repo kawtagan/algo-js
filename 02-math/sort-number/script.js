4ce4873306f2f6d1bc6bc3c4be73fe9db4580bac
kawtagan <89374054+kawtagan@users.noreply.github.com>
2021-09-22 10:27:59 +0200

    Add files via upload

---
+++ 02-math/sort-number/script.js
@@ -0,0 +1,15 @@
+// 02-maths/04-sort-numbers/script.js - 2.4: Sorting the numbers
+
+(() => {
+
+    document.getElementById("numbers").value;
+
+    document.getElementById("run").addEventListener("click", () => {
+
+    const points = [2, 4, 14, 10, 90, 23, 16];
+
+             points.sort(function(a, b){return a-b});
+
+    });
+
+})();
