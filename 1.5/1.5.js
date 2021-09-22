4835a93193859e18d89baff48e4d5a55768ce1d2
kawtagan <89374054+kawtagan@users.noreply.github.com>
2021-09-22 12:03:13 +0200

    Delete 1.5 directory

--- 1.5/1.5.js
+++
@@ -1,4 +0,0 @@
-let '1.5' = readlineSync.question('Can you give me the first decimal number please ?');
-let '5.3' = readlineSync.question('Can you give me the second decimal number please ?');
-
-console.log( parseInt(1.5) * 5.3 );
