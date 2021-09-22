d5905ffecb629a3d53cd9acd595346e2a799c1af
kawtagan <89374054+kawtagan@users.noreply.github.com>
2021-09-22 12:03:20 +0200

    Delete 1.6 directory

--- 1.6/1.6.js
+++
@@ -1,6 +0,0 @@
-const readlineSync = require("readline-sync");
-
-let numbers = readlineSync.question('Can you give me two numbers ?');
-console.log( numbers + "10 and 20");
-
-console.log('10' / '20');
